2ac7d39105ce35de1416aabc0c4b9f12c650cfac
YutoNakano <yuto.44.bb@gmail.com>
2019-09-13 22:06:17 +0900

    initial commit

---
+++ conpass_client/Search/SearchEventViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  conpass_client
+//
+//  Created by 中野湧仁 on 2019/09/13.
+//  Copyright © 2019 中野湧仁. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+final class SearchEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
---
+++ conpass_clientTests/conpass_clientTests.swift
@@ -0,0 +1,34 @@
+//
+//  conpass_clientTests.swift
+//  conpass_clientTests
+//
+//  Created by 中野湧仁 on 2019/09/13.
+//  Copyright © 2019 中野湧仁. All rights reserved.
+//
+
+import XCTest
+@testable import conpass_client
+
+class conpass_clientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
