a8c0a295e62304d291f92d9d1b8aafe12661da0d
YutoNakano <yuto.44.bb@gmail.com>
2019-09-13 23:39:28 +0900

    add:api通信をするためAlamofireを導入

--- conpass_client/TopEvent/TopEventModel.swift
+++ conpass_client/TopEvent/TopEventModel.swift
@@ -1,16 +1,19 @@
 //
-//  TopTableViewCell.swift
+//  TopEventModel.swift
 //  conpass_client
 //
 //  Created by 中野湧仁 on 2019/09/13.
 //  Copyright © 2019 中野湧仁. All rights reserved.
 //
 
-import UIKit
+import Foundation
 import RxSwift
+import Alamofire
 
-final class TopTableViewCell: UITableViewCell {
-    @IBOutlet weak var titleLabel: UILabel!
+protocol TopEventModelProtocol {
     
+}
+
+final class TopEventModel: TopEventModelProtocol {
     
 }
--- conpass_client/TopEvent/TopEventViewController.swift
+++ conpass_client/TopEvent/TopEventViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RxSwift
 
-final class TopViewController: UIViewController {
+final class TopEventViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView! {
         didSet {
@@ -24,18 +24,18 @@ final class TopViewController: UIViewController {
     }
 }
 
-extension TopViewController: UITableViewDataSource {
+extension TopEventViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: TopTableViewCell = tableView.dequeueReusableCell(withIdentifier: "TopTableViewCell", for: indexPath) as! TopTableViewCell
-        return cell 
+        let cell: TopEventTableViewCell = tableView.dequeueReusableCell(withIdentifier: "TopEventTableViewCell", for: indexPath) as! TopEventTableViewCell
+        return cell
     }
     
 }
 
-extension TopViewController: UITableViewDelegate {
+extension TopEventViewController: UITableViewDelegate {
     
 }
